4f8aaaca132d32ee667d75ab5f05b3eb5681a24e
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-05-09 17:11:56 -0400

    Attaching pkg

---
+++ static/pkg/throwing_ferris_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5b106eb5ac297239(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h91a413e340ac6397(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
